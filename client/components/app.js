9435c862992d28553e111536b991227202496d3d
Steve Dakh <slavik0329@gmail.com>
2017-04-22 08:47:47 -0400

    initial commit

---
+++ client/components/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+	      Redux Express Starter
+		    {/*{this.props.children}*/}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['babel-polyfill', './client/index.js'],
+  output: {
+    path: './public',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({ // Give access to the NODE_ENV in the bundled code
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public',
+    inline: true
+  }
+};
